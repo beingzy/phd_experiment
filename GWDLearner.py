5e3a216818de93aaf5980e1c15933e4782cccf1f
Yi Zhang <yi.zhang@atos.net>
2015-06-28 21:08:12 -0400

    add notebook to explore dr.huagn's data, wrap all pre-developed functions into GWDLearner.py

---
+++ GWDLearner.py
@@ -0,0 +1,265 @@
+from scipy.stats import rayleigh
+from scipy.stats import ks_2samp
+from numpy import linspace
+from numpy.random import choice
+from networkx import Graph
+from learning_dist_metrics.ldm import LDM
+from learning_dist_metrics.dist_metrics import weighted_euclidean
+
+
+def user_grouped_dist(user_id, weights, profile_df, friends_networkx):
+    """ Calculate distances between a user and whose friends
+        and distance between a user and whose non-friends.
+        The groupped distance vector will be output.
+
+    Parameters:
+    ----------
+    * user_id: {integer}, the target user's ID
+    * weights: {vector-like, float}, the vector of feature weights which
+        is extracted by LDM().fit(x, y).get_transform_matrix()
+    * profile_df: {matrix-like, pandas.DataFrame}, user profile dataframe
+        with columns: ["ID", "x0" - "xn"]
+    * friends_networkx: {networkx.Graph()}, Graph() object from Networkx
+        to store the relationships informat
+    Returns:
+    -------
+    res: {list, list of integers}, a list of two lists, which store the distances
+        of either friends and non-friends separately.
+
+    Examples:
+    ---------
+    weights = ldm().fit(df, friends_list).get_transform_matrix()
+    profile_df = users_df[ ["ID"] + cols ]
+    user_dist = user_grouped_dist(user_id = 0, weights = weights
+        , profile_df, friends_df)
+    print user_dist["friends"]
+    print user_dist["nonfriends"]
+    """
+    cols = [col for col in profile_df.columns if col is not "ID"]
+    # get the user profile information of the target users
+    user_profile = profile_df.ix[profile_df.ID == user_id, cols].as_matrix()
+    # get the user_id of friends of the target user
+    friends_ls = friends_networkx.neighbors(user_id)
+    all_ids = profile_df.ID
+    non_friends_ls = [u for u in all_ids if u not in friends_ls + [user_id]]
+
+    sim_dist_vec = []
+    for f_id in friends_ls:
+        friend_profile = profile_df.ix[profile_df.ID == f_id, cols].as_matrix()
+        the_dist = weighted_euclidean(user_profile, friend_profile, weights)
+        sim_dist_vec.append(the_dist)
+
+    diff_dist_vec = []
+    for nf_id in non_friends_ls:
+        nonfriend_profile = profile_df.ix[profile_df.ID == nf_id, cols].as_matrix()
+        the_dist = weighted_euclidean(user_profile, nonfriend_profile, weights)
+        diff_dist_vec.append(the_dist)
+
+    res = [sim_dist_vec, diff_dist_vec]
+    return res
+
+
+def user_dist_kstest(sim_dist_vec, diff_dist_vec,
+                     fit_rayleigh=False, _n=100):
+						 
+    """ Test the goodness of a given weights to defferentiate friend distance
+        distributions and non-friend distance distributions of a given user.
+        The distance distribution is considered to follow Rayleigh distribution.
+
+    Parameters:
+    ----------
+    sim_dist_vec: {vector-like (list), float}, distances between friends
+                  and the user
+    diff_dist_vec: {vector-like (list), float}, distances between non-fri
+                   -ends and the user
+    fit_rayleigh: {boolean}, determine if fit data into Rayleigth distri
+                  -bution
+    _n: {integer}, number of random samples generated from estimated
+        distribution
+
+    Returns:
+    -------
+    * res: {float}: p-value of ks-test with assumption that distances follow
+            Rayleigh distribution.
+
+    Examples:
+    ---------
+    pval = user_dist_kstest(sim_dist_vec, diff_dist_vec)
+    """
+    # is_valid = (len(sim_dist_vec) >= min_nobs) & \
+    #           (len(diff_dist_vec) >= min_nobs) # not used yet
+    if fit_rayleigh:
+        friend_param = rayleigh.fit(sim_dist_vec)
+        nonfriend_param = rayleigh.fit(diff_dist_vec)
+
+        samp_friend = rayleigh.rvs(friend_param[0], friend_param[1], _n)
+        samp_nonfriend = rayleigh.rvs(nonfriend_param[0], nonfriend_param[1], _n)
+
+        # ouput p-value of ks-test
+        res = ks_2samp(samp_friend, samp_nonfriend)[1]
+    else:
+        res = ks_2samp(sim_dist_vec, diff_dist_vec)[1]
+		
+    return res
+
+
+def users_filter_by_weights(weights, profile_df, friends_networkx,
+                            pval_threshold=0.5, 
+                            mutate_rate=0.4,
+                            min_friend_cnt=10, 
+                            users_list=None,
+                            fit_rayleigh=False, 
+                            _n=1000,
+                            is_debug=False):
+    """ Split users into two groups, "keep" and "mutate", with respect to
+        p-value of the ks-test on the null hypothesis that the distribution of
+        friends' weighted distance is not significantly different from the
+        couterpart for non-friends. Assume the weighted distances of each group
+        follow Rayleigh distribution.
+
+    Parameters:
+    ----------
+    weights: {vector-like, float}, the vector of feature weights which
+        is extracted by LDM().fit(x, y).get_transform_matrix()
+    users_list: {vector-like, integer}, the list of user id
+    profile_df: {matrix-like, pandas.DataFrame}, user profile dataframe
+        with columns: ["ID", "x0" - "xn"]
+    friends_networkx: {networkx.Graph()}, Graph() object from Networkx to store
+        the relationships information
+    pval_threshold: {float}, the threshold for p-value to reject hypothesis
+    min_friend_cnt: {integer}, drop users whose total of friends is less than
+       this minimum count
+    mutate_rate: {float}, a float value [0 - 1] determine the percentage of
+       bad_fits member sent to mutation
+    fit_rayleigh: {boolean}, determine if fit data into Rayleigth distri
+                  -bution
+    _n: {integer}, number of random samples generated from estimated
+        distribution
+    is_debug: {boolean}, to control if it yeilds by-product information
+
+    Returns:
+    -------
+    res: {list} grouped list of user ids
+        res[0] stores all users whose null hypothesis does not holds;
+        res[1] stores all users whose null hypothesis hold null hypothesis,
+        given weights, distance distribution of all friends is significantly
+        different from distance distribution of all non-friends
+
+    Examples:
+    --------
+    weights = ldm().fit(df, friends_list).get_transform_matrix()
+    profile_df = users_df[["ID"] + cols]
+    grouped_users = users_filter_by_weights(weights,
+                       profile_df, friends_df, pval_threshold = 0.10,
+                       min_friend_cnt = 10)
+
+    Notes:
+    -----
+    min_friend_cnt is not implemented
+    """
+    # all_users_ids = list(set(profile_df.ID))
+    # users_list
+    # container for users meeting different critiria
+    pvals = []
+    if users_list is None:
+        users_list = list(profile_df.ix[:, 0])
+
+    for uid in users_list:
+        res_dists = user_grouped_dist(uid, weights, profile_df, friends_networkx)
+        pval = user_dist_kstest(res_dists[0], res_dists[1], fit_rayleigh, _n)
+        pvals.append(pval)
+
+    sorted_id_pval = sorted(zip(users_list, pvals), key=lambda x: x[1])
+    good_fits = [i for i, p in sorted_id_pval if p < pval_threshold]
+    bad_fits = [i for i, p in sorted_id_pval if p >= pval_threshold]
+    
+    if len(bad_fits) > 0:
+        mutate_size = np.ceil(len(bad_fits) * mutate_rate)
+        mutate_size = max(int(mutate_size), 1)
+        id_retain = good_fits + bad_fits[mutate_size:]
+        id_mutate = bad_fits[:mutate_size]
+    else:
+        id_retain = good_fits
+        id_mutate = bad_fits
+        
+    if is_debug is True:
+        res = [id_retain, id_mutate, sorted_id_pval]
+    else:
+        res = [id_retain, id_mutate]
+    return res
+
+
+def ldm_train_with_list(users_list, profile_df, friends_df, retain_type=0):
+    """ learning distance matrics with ldm() instance, provided with selected
+        list of users.
+
+    Parameters:
+    -----------
+    users_list: {vector-like, integer}, the list of user id
+    profile_df: {matrix-like, pandas.DataFrame}, user profile dataframe
+        with columns: ["ID", "x0" - "xn"]
+    friends_df: {matrix-like, pandas.DataFrame}, pandas.DataFrame store pair of
+        user ID(s) to represent connections with columns: ["uid_a", "uid_b"]
+    friends_networkx: {networkx.Graph()}, Graph() object from Networkx to store
+        the relationships information
+    retain_type: {integer}, 0, adopting 'or' logic by keeping relationship in
+        friends_df if either of entities is in user_list 1, adopting 'and'
+        logic
+
+    Returns:
+    -------
+    res: {vector-like, float}, output of ldm.get_transform_matrix()
+
+    Examples:
+    ---------
+    new_dist_metrics = ldm_train_with_list(user_list, profile_df, friends_df)
+    """
+	
+    ldm = LDM()
+	
+    if retain_type == 0:
+        friends_df = friends_df.ix[friends_df.uid_a.isin(users_list) |
+                                   friends_df.uid_b.isin(users_list)]
+    else:
+        friends_df = friends_df.ix[friends_df.uid_a.isin(users_list) &
+                                   friends_df.uid_b.isin(users_list)]
+
+    try:
+	    profile_df = profile_df.drop("ID")
+		
+    ldm.fit(profile_df, friends_df.pair.as_matrix())
+    return ldm.get_transform_matrix()
+	
+def hyper_parameter_tester(weights_a, weights_b, fit_rayleigh, num):
+    
+    num_friends = []
+    num_nonfriends = []
+    ks_pvals_right = []
+    ks_pvals_wrong = []
+
+    for uid in tg0_ids: 
+        # Compare the distribution of a user's distances of all of his/her friends
+        # against the distribuiton of a users's distances of all of his/her non-friends,
+        # The collection of non-friends may include those users of two categories with
+        # respect to their relationships to the target user: 
+        # a. the users who are not likened by the target users 
+        # b. the users who are likely to be befriended by the users however
+        #    the users do not have a change to be exposed to her/him.
+        sim_dists, diff_dists = user_grouped_dist(uid, weights_a, profile_df, fnx)
+        pval = user_dist_kstest(sim_dists, diff_dists, fit_rayleigh=fit_rayleigh, _n = num)
+        ks_pvals_right.append(pval)
+    
+        sim_dists, diff_dists = user_grouped_dist(uid, weights_b, profile_df, fnx)
+        pval = user_dist_kstest(sim_dists, diff_dists, fit_rayleigh=fit_rayleigh, _n = num)
+        ks_pvals_wrong.append(pval)
+    
+        num_friends.append(len(sim_dists))
+        num_nonfriends.append(len(diff_dists))
+    
+    res_report = pd.DataFrame({"ID": tg0_ids, 
+		                       "num_friends": num_friends, 
+                               "num_nonfriends": num_nonfriends, 
+                               "true_pval": ks_pvals_right,
+                               "wrong_pval": ks_pvals_wrong})
+							   
+    return res_report
